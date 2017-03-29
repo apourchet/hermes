aced14d8d3f85c0a0b1e4ab016a65cc0b9c3d44a
Antoine Pourchet <antoine.pourchet@gmail.com>
2017-03-29 00:14:07 -0400

    Error not nested

--- error.go
+++ error.go
@@ -1,12 +1,9 @@
 package hermes
 
-import "encoding/json"
-
 type Error struct {
 	Message string
 }
 
 func (e *Error) Error() string {
-	content, _ := json.Marshal(e)
-	return string(content)
+	return e.Message
 }
